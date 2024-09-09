6a7b80d1f768fdb0801e129480e4488322c4f89f
Leandro Fonseca Daher <137357645+LehFonseca@users.noreply.github.com>
2024-09-09 18:30:21 -0300

    Add files via upload

---
+++ Criando um menu de opções.py
@@ -0,0 +1,34 @@
+from time import sleep
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+opção = 0
+while opção != 5:
+    print('''    [ 1 ] Somar
+    [ 2 ] Multiplicar
+    [ 3 ] Maior
+    [ 4 ] Novos números
+    [ 5 ] Sair do programa''')
+    opção = int(input('>>>>> Qual é a sua opção? '))
+    if opção == 1:
+        soma = n1 + n2
+        print(f'A soma entre {n1} + {n2} é {soma}.')
+    elif opção == 2:
+        produto = n1 * n2
+        print(f'O resultado de {n1} x {n2} é {produto}.')
+    elif opção == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print(f'Entre {n1} e {n2} o maior valor é {maior}.')
+    elif opção == 4:
+        print('Informe os números novamente:')
+        n1 = int(input('Primeiro valor: '))
+        n2 = int(input('Segundo valor: '))
+    elif opção == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. tente novamente.')
+    print('-=-' * 10)
+    sleep(2)
+print('Fim do programa! Volte sempre!')
